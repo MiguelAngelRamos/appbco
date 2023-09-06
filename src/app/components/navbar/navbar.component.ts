6dd0375310dc4848f0bd7b0131f758feb6c3456e
Miguel Ramos <return.infox@gmail.com>
2023-09-06 19:18:55 -0300

    actualizar el navbar con opciones

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  public opciones:any = [
+    { path: '/home', name: 'Home'},
+    { path: '/depositos', name: 'Depositos'},
+    { path: '/prestamos', name: 'Prestamos'},
+    { path: '/giros', name: 'Giros'},
+    { path: '/cta-corriente', name: 'Cuenta corriente'},
+  ];
+
+  public title: string = "Angular Bco";
+
 }
