2912f7f7f8dc6e61821e63127c6d82cf82803061
Miguel Ramos <return.infox@gmail.com>
2023-09-06 20:28:09 -0300

    codigo actualizado del navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DepositoComponent } from './pages/deposito/deposito.component';
+import { PrestamosComponent } from './pages/prestamos/prestamos.component';
+import { CuentaCorrienteComponent } from './pages/cuenta-corriente/cuenta-corriente.component';
+import { GirosComponent } from './pages/giros/giros.component';
 
-const routes: Routes = [];
+ //* http://localhost:4200/home
+ //* http://localhost:4200/depositos
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'depositos', component: DepositoComponent },
+  { path: 'prestamos', component: PrestamosComponent },
+  { path: 'cta-corriente', component: CuentaCorrienteComponent },
+  { path: 'giros', component: GirosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DepositoComponent } from './pages/deposito/deposito.component';
+import { PrestamosComponent } from './pages/prestamos/prestamos.component';
+import { GirosComponent } from './pages/giros/giros.component';
+import { CuentaCorrienteComponent } from './pages/cuenta-corriente/cuenta-corriente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    DepositoComponent,
+    PrestamosComponent,
+    GirosComponent,
+    CuentaCorrienteComponent
   ],
   imports: [
     BrowserModule,
