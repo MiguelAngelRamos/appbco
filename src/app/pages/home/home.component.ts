fbb8434912e4c5c6de2267fbcf0909f959f3df1f
Miguel Ramos <return.infox@gmail.com>
2023-09-07 20:07:07 -0300

    app bco finalizada

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,9 +9,34 @@ export class HomeComponent {
 
   public title: string = 'Mi Cuenta de Banco';
   public headersTable: string[] = ["Saldo", "Estado"];
-  public saldo: number = 10;
+  public saldo: number = 0;
   public statusNow: string = "Status off";
   public status: boolean = false;
 
+
+  public aumentarSaldo() {
+    this.saldo = this.saldo + 1000;
+    // this.saldo++;
+  }
+  public disminuirSaldo() {
+    this.saldo = this.saldo - 1000;
+    // this.saldo--;
+  }
+
+  public cambiarEstado() {
+
+    this.status = !this.status;
+    if(this.status) {
+      //* se ejecuta esta parte "true"
+      this.statusNow = 'Active';
+    } else {
+      this.statusNow = 'Disabled';
+    }
+
+    //* true o false
+    //* !true => false
+    //* !false => true
+
+  }
 
 }
