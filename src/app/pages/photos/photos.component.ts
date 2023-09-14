35a41e56a1ad85dc5c5266536fda866ec1fc956f
Miguel Ramos <return.infox@gmail.com>
2023-09-14 20:01:32 -0300

    fotografias terminadas

--- src/app/pages/photos/photos.component.ts
+++ src/app/pages/photos/photos.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IPhoto } from 'src/app/interfaces/IPhoto';
+import { DataPhotoService } from 'src/app/services/data-photo.service';
 
 @Component({
   selector: 'app-photos',
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
-export class PhotosComponent {
+export class PhotosComponent implements OnInit {
+
+  public photos:IPhoto[] = [];
+  constructor(private httpPhotoService: DataPhotoService) {
+
+  }
+  //* control + .
+  ngOnInit(): void {
+    this.httpPhotoService.getPhotos().subscribe(photos => {
+      this.photos = photos;
+    });
+  }
+
 
 }
